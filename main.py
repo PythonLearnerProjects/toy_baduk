a9927ccc5d00cdc836dbcd3bd5b105bcb2a150ce
Pedro Castro <pedrocastro@Pedros-MacBook-Pro.local>
2020-01-21 19:23:48 +0000

    Fix

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from curses import wrapper
+import curses
 from util import Display, eprint
 from time import sleep
 from board import Board
-import sys
 
 
 def main(screen):
     # Create the display object. This is found in util.py
+    x=3
+    y=3
     display = Display(screen)
     board = Board()
     message = 'Hello'
@@ -27,11 +29,21 @@ def main(screen):
         # https://docs.python.org/3/library/curses.html#constants
 
         input_char = display.get_input()
-        display.set_cursor((3,3))
+        display.set_cursor((x,y))
 
+        if input_char == curses.KEY_RIGHT:
+            x=x+1
+        elif input_char == curses.KEY_DOWN:
+            y=y+1
+        elif input_char == curses.KEY_UP:
+            y=y-1
+        elif input_char == curses.KEY_LEFT:
+            x=x-1
+
+        display.set_cursor((x,y))
         # These may help you figure out what is going on
         eprint("This appears in the error log.", input_char)
-        message = key_template.format(input_char, chr(input_char))
+        message = key_template.format(input_char, curses.KEY_UP)
 
         #
         ######################################################
